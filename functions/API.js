b13ea17fe214868b63c4e15c335b9bcdf29f9ce1
Rafael-Puerta <rafapc1992@gmail.com>
2023-02-27 18:26:48 +0100

    test new conditional

--- functions/API.js
+++ functions/API.js
@@ -63,7 +63,7 @@ async function getProfile(req,res){
     return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
   }
 
-  if(phone==NaN) return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
+  if(Number.isNaN(phone)) return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
 
   try {
     var data = await utils.queryDatabase(`SELECT * FROM Usuaris WHERE phone=${phone};`)
@@ -97,7 +97,7 @@ async function singup(req,res){
     return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
   }
 
-  if(phone==NaN) return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
+  if(Number.isNaN(phone)) return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
 
   try {
     var data = await utils.queryDatabase(`SELECT * FROM Usuaris WHERE phone='${receivedPOST.phone}';`)
@@ -138,7 +138,7 @@ async function setup_payment(req,res){
     return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
   }
 
-  if(phone==NaN) return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
+  if(Number.isNaN(phone)) return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
 
   try {
     amount=Number.parseInt(receivedPOST.amount)
@@ -197,7 +197,7 @@ async function start_payment(req,res){
     return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
   }
 
-  if(phone==NaN) return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
+  if(Number.isNaN(phone)) return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
 
   try {
     var data = await utils.queryDatabase(`SELECT * FROM Usuaris WHERE phone='${phone}';`)
@@ -241,7 +241,7 @@ async function finish_payment(req,res){
     return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
   }
 
-  if(phone==NaN) return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
+  if(Number.isNaN(phone)) return res.end(JSON.stringify({ status: "KO", result: "Phone is invalid" }))
 
   try {
     var data = await utils.queryDatabase(`SELECT * FROM Usuaris WHERE phone='${phone}';`)
